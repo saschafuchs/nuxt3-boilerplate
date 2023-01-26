33c171e8632ea652093cf2a3dd624d556a3654e6
saschafuchs <sascha.fuchs@webpros.com>
2023-01-26 13:33:06 +0100

    build: added histoire

---
+++ components/base/TestComp/TestComp.story.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import TestComp from './TestComp.vue'
+
+const source = `
+<TestComp :msg="msg" />
+`
+</script>
+
+<template>
+  <Story
+    :source="source"
+    icon="lucide:cpu"
+    :layout="{ type: 'single', iframe: false }"
+    title="Test component story"
+  >
+    <TestComp />
+  </Story>
+</template>
---
+++ histoire.setup.ts
@@ -0,0 +1,6 @@
+import './assets/css/tailwind.css'
+
+export function setupVue3 () {
+  // app.provide('test', 'hello')
+  // app.use(...)
+}
